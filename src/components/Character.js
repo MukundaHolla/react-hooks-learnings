249171dbf71364943589fe1c9c1f417ac5a35716
MukundaHolla <kanda05holla@gmail.com>
2019-10-20 19:27:29 +0530

    using useEffect() for componentDidUpdate() and componentWillUnmount() and React.memo()

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Summary from './Summary';
+import { ReactComponent } from '*.svg';
 
-class Character extends Component {
-  state = { loadedCharacter: {}, isLoading: false };
+const Character = props => {
+  //state = { loadedCharacter: {}, isLoading: false };
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return (
-      nextProps.selectedChar !== this.props.selectedChar ||
-      nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
-      nextState.isLoading !== this.state.isLoading
-    );
-  }
+  const [loadedCharacter, setLoadedCharacter] = useState({});
 
-  componentDidUpdate(prevProps) {
-    console.log('Component did update');
-    if (prevProps.selectedChar !== this.props.selectedChar) {
-      this.fetchData();
-    }
-  }
+  const [isLoading, setIsLoading] = useState(false);
 
-  componentDidMount() {
-    this.fetchData();
-  }
+  console.log('Rendering..');
 
-  fetchData = () => {
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate');
+  //   return (
+  //     nextProps.selectedChar !== this.props.selectedChar ||
+  //     nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
+  //     nextState.isLoading !== this.state.isLoading
+  //   );
+  // }
+
+  const fetchData = () => {
     console.log(
-      'Sending Http request for new character with id ' +
-        this.props.selectedChar
+      'Sending Http request for new character with id ' + props.selectedChar
     );
-    this.setState({ isLoading: true });
-    fetch('https://swapi.co/api/people/' + this.props.selectedChar)
+    setIsLoading(true);
+    // this.setState({ isLoading: true });
+    fetch('https://swapi.co/api/people/' + props.selectedChar)
       .then(response => {
         if (!response.ok) {
           throw new Error('Could not fetch person!');
@@ -40,7 +36,7 @@ class Character extends Component {
       })
       .then(charData => {
         const loadedCharacter = {
-          id: this.props.selectedChar,
+          id: props.selectedChar,
           name: charData.name,
           height: charData.height,
           colors: {
@@ -50,36 +46,86 @@ class Character extends Component {
           gender: charData.gender,
           movieCount: charData.films.length
         };
-        this.setState({ loadedCharacter: loadedCharacter, isLoading: false });
+        setIsLoading(false);
+        setLoadedCharacter(loadedCharacter);
+        // this.setState({ loadedCharacter: loadedCharacter, isLoading: false });
       })
       .catch(err => {
         console.log(err);
+        setIsLoading(false);
       });
   };
 
-  componentWillUnmount() {
-    console.log('Too soon...');
+  // why sending array param?
+  // because i want to re-run when selectedChar dropdown value changes.
+  // so i have to specify the variable name inside that array we pass as second param
+  // clean-up can also be done using useEffect
+  // it can return another function => this code inside will run right before useEffect will run the next time. its a clean-up function
+
+  // ------------- 2 IMPORTANT THINGS HANDLED IN useEffect
+  // componentDidUpdate()
+  // componentWillUnmount()
+  // but how do we handle shouldComponentUpdate() work using hooks?
+  // ANSWER => USE React.memo()
+
+  useEffect(() => {
+    fetchData();
+    return () => {
+      console.log('Cleaning up ..');
+    };
+  }, [props.selectedChar]);
+
+  // Soooooo here's a new stuff
+  // when you want to run clean-up only once when component will unmount. we can have another useEffect with empty return and empty second argument
+
+  useEffect(() => {
+    return () => {
+      console.log('component did unMount');
+    };
+  }, []);
+
+  // componentDidUpdate(prevProps) {
+  //   console.log('Component did update');
+  //   if (prevProps.selectedChar !== this.props.selectedChar) {
+  //     this.fetchData();
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.fetchData();
+  // }
+
+  // componentWillUnmount() {
+  //   console.log('Too soon...');
+  // }
+
+  let content = <p>Loading Character...</p>;
+
+  if (!isLoading && loadedCharacter.id) {
+    content = (
+      <Summary
+        name={loadedCharacter.name}
+        gender={loadedCharacter.gender}
+        height={loadedCharacter.height}
+        hairColor={loadedCharacter.colors.hair}
+        skinColor={loadedCharacter.colors.skin}
+        movieCount={loadedCharacter.movieCount}
+      />
+    );
+  } else if (!isLoading && !loadedCharacter.id) {
+    content = <p>Failed to fetch character.</p>;
   }
+  return content;
+};
 
-  render() {
-    let content = <p>Loading Character...</p>;
-
-    if (!this.state.isLoading && this.state.loadedCharacter.id) {
-      content = (
-        <Summary
-          name={this.state.loadedCharacter.name}
-          gender={this.state.loadedCharacter.gender}
-          height={this.state.loadedCharacter.height}
-          hairColor={this.state.loadedCharacter.colors.hair}
-          skinColor={this.state.loadedCharacter.colors.skin}
-          movieCount={this.state.loadedCharacter.movieCount}
-        />
-      );
-    } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
-      content = <p>Failed to fetch character.</p>;
-    }
-    return content;
-  }
-}
-
-export default Character;
+// what is React.memo() ?
+// this is called wrapping your entire component inside a memo.
+// it was introduced in react 16.6 which memoizes the component
+// which means it basically it and re-renders when the props changes
+// we can pass second argument also.
+// which is function which should return true if the props are equal(dont re-render)
+// and return false if it should re-render
+// export default React.memo(Character, (prevProps, nextProps) => {
+//   return nextProps.selectedChar === prevProps.selectedChar;
+// });
+export default React.memo(Character);
